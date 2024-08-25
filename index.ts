9580840351c85fd810dcbf1f82fc46b5fbbd5130
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-25 15:02:51 -0400

    chore: use cookie

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ const response = await fetch(
   "https://hackclub.slack.com/api/functions.workflows.steps.update",
   {
     headers: {
-      Cookie: `utm=%7B%7D; b=.664b361ec72b83efa171b1003287b3ee; ssb_instance_id=f7fd8100-c66d-49aa-8e2b-fb461f91a998; OptanonConsent=isGpcEnabled=1&datestamp=Sun+Aug+25+2024+13%3A36%3A57+GMT-0400+(Eastern+Daylight+Time)&version=202402.1.0&browserGpcFlag=1&isIABGlobal=false&hosts=&consentId=ea92d818-f691-45d2-a667-5414c74d3e6a&interactionCount=2&isAnonUser=1&landingPath=NotLandingPage&groups=1%3A1%2C3%3A1%2C2%3A1%2C4%3A0&AwaitingReconsent=false&geolocation=%3B; _lc2_fpi=e00b11ac9c9b--01j3exjw7zh8jmbgqrfdqkw162; _lc2_fpi_me…ct_page=1; OptanonAlertBoxClosed=2024-07-25T00:53:19.824Z; tz=-240; web_cache_last_updatedc4e4e9bc280f7567b9a3e67d319b735e=1724589147373; shown_download_ssb_modal=1; show_download_ssb_banner=1; no_download_ssb_banner=1; d-s=1721868798; x=664b361ec72b83efa171b1003287b3ee.1724607379; uc=xoxd-nzuWviqoVRuY%2B64CDHUqZTVlgBUXsNpk6UHEmlmluWEUelUbr%2FYWkh7iNXsVcUjAI7LkW%2F1GyVOfPGjhRr5yzAcTdsK%2Bk3wvW3FyoawPogCHcr1NVZmrdGyQJ2qgWWXz3IBtb%2BYHo0vFTplrOkrkYQC6NnnL4Ou0i8UR18HX%2B8zCCRhWY1nc6TVlXfNVTtjZFP%2FGKk71TnM%3D`,
+      Cookie: process.env.SLACK_COOKIE!,
     },
     body: formData,
     method: "POST",
